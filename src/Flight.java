77bf11b9d11b782bcdbba869789869140b4cbd54
sell488 <sell488sftoday@gmail.com>
2018-02-13 13:29:12 -0600

    Added if statements and getters/setters

--- src/Flight.java
+++ src/Flight.java
@@ -1,2 +1,23 @@
 public class Flight {
+
+    int flyNum1;
+    int bagsChecked1;
+
+    public int getFlyNum1() {
+        return flyNum1;
+    }
+
+    public void setFlyNum1(int flyNum1) {
+        this.flyNum1 = flyNum1;
+    }
+
+    public int getBagsChecked1() {
+        return bagsChecked1;
+    }
+
+    public void setBagsChecked1(int bagsChecked1) {
+        this.bagsChecked1 = bagsChecked1;
+    }
+
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,54 @@ import javax.swing.JOptionPane;
 
 public class Main {
 
-    int passengers;
-    int checkedBags;
-    int freeBags;
-    int seats = 100;
-    int flightNum1 = 3210;
-    int flightNum2 = 2594;
-    int flightNum3 = 5867;
 
-    String flightNumInput1 = JOptionPane.showInputDialog("Enter your flight number");
+    public static void main(String[] args) {
+
+        int passengers;
+        int checkedBags;
+        int freeBags;
+        int space1 = 100;
+        int space2 = 300;
+        int space3 = 125;
+        int seats = 100;
+        int flightNum1 = 3210;
+        int flightNum2 = 2594;
+        int flightNum3 = 5867;
+
+        String flightNumInput1 = JOptionPane.showInputDialog("Enter your flight number");
+        int flightNumSet1 = Integer.parseInt(flightNumInput1);
+
+        boolean done1 = false;
+        while (!done1) {
+            if (flightNumInput1 == null) {
+                System.out.println("No Flight Number Inputted");
+            }
+            else if(flightNumSet1 == flightNum1) {
+
+                String checkBagsIn1 = JOptionPane.showInputDialog("Enter number of checked bags");
+                int NumChekcBagsIn1 = Integer.parseInt(checkBagsIn1);
 
 
 
+            }
+
+            else if(flightNumSet1 == flightNum2) {
+
+            }
+
+            else if(flightNumSet1 == flightNum3) {
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
 
 }
