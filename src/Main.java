5d503869a72d7301748255a97a41975c845b83dd
sell488 <sell488sftoday@gmail.com>
2018-02-13 12:57:03 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    int passengers;
+    int checkedBags;
+    int freeBags;
+    int seats = 100;
+    int flightNum1 = 3210;
+    int flightNum2 = 2594;
+    int flightNum3 = 5867;
+
+    String flightNumInput1 = JOptionPane.showInputDialog("Enter your flight number");
+
+
+
+
+}
