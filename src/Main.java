e237bc7c32cd7a1f13461adf0c340d21c299b61c
sell488 <sell488sftoday@gmail.com>
2018-02-16 11:15:46 -0600

    Loops

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,16 @@ public class Main {
         int flightNum1 = 3210;
         int flightNum2 = 2594;
         int flightNum3 = 5867;
-        int NumCheckBagsIn1 = 10;
+        int NumCheckBagsIn1;
+        int NumCheckBagsIn2;
+        int NumCheckBagsIn3;
         boolean done1 = false;
         boolean flightDone1 = false;
         int flightNumSet1 = -1;
 
 
         ArrayList <Flight> flightList1 = new ArrayList<>();
-
+        while (!done1) {
         String flightNumInput1 = JOptionPane.showInputDialog("Enter your flight number");
 
 
@@ -37,27 +39,50 @@ public class Main {
         }
 
 
-        while (!done1) {
+
             if (flightNumSet1 == -1) {
                 System.out.println("No Flight Number Inputted");
                 done1 = true;
             } else if (flightNumSet1 == flightNum1 && !flightDone1) {
 
-                String checkBagsIn1 = JOptionPane.showInputDialog("Enter number of checked bags");
+                String checkBagsIn1 = JOptionPane.showInputDialog("Enter number of checked bags for flight number " + flightNum1);
                 NumCheckBagsIn1 = Integer.parseInt(checkBagsIn1);
 
                 Flight flight1 = new Flight(flightNum1, NumCheckBagsIn1, space1);
-                int spaceLeft = flight1.getLeftOver();
-                System.out.println(spaceLeft);
+                int spaceLeft1 = flight1.getLeftOver();
+                System.out.println(spaceLeft1);
 
                 flightDone1 = true;
                 done1 = true;
 
 
-            } else if (flightNumSet1 == flightNum2) {
+            } else if (flightNumSet1 == flightNum2 && !flightDone1) {
 
-            } else if (flightNumSet1 == flightNum3) {
+                String checkBagsIn2 = JOptionPane.showInputDialog("Enter number of checked bags for flight number " + flightNum2);
+                NumCheckBagsIn2 = Integer.parseInt(checkBagsIn2);
 
+                Flight flight2 = new Flight(flightNum2, NumCheckBagsIn2, space2);
+                int spaceLeft2 = flight2.getLeftOver();
+                System.out.println(spaceLeft2);
+
+                flightDone1 = true;
+                done1 = true;
+
+            } else if (flightNumSet1 == flightNum3 && !flightDone1) {
+
+                String checkedBagsIn3 = JOptionPane.showInputDialog("Enter number of checked bags for flight number " + flightNum3);
+                NumCheckBagsIn3 = Integer.parseInt(checkedBagsIn3);
+
+                Flight flight3 = new Flight(flightNum3, NumCheckBagsIn3, space3);
+                int spaceLeft3 = flight3.getLeftOver();
+                System.out.println(spaceLeft3);
+
+                flightDone1 = true;
+                done1 = true;
+
+            } else {
+                System.out.println("Not a valid flight");
+                done1 = true;
             }
 
         }
